75f29157734e97d6c6fb57182ce766ce4e5b7bda
cxysky8 <39475303+cxysky8@users.noreply.github.com>
2018-11-15 21:33:15 +0800

    Update pa01.py

--- pa01.py
+++ pa01.py
@@ -2,6 +2,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+#适合爬取静态页面
 html=urlopen("http://jr.jd.com")
 bs_obj=BeautifulSoup(html.read(),'html.parser')
 text_list=bs_obj.find_all("a","nav-item-primary")
